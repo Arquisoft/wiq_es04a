cd4220148dce872e87200def4cd1ba955ee43004
samuel <samul@LAPTOP-0TGJLS0A>
2024-02-02 19:25:38 +0100

    Improvement and fix and documentation

--- investigate/wikidata_preg.js
+++ investigate/wikidata_preg.js
@@ -1,11 +1,21 @@
 import fetch from 'node-fetch';
 
 // Código de ejemplo para generar preguntas usando la plantilla: ¿Cuál es la población de x? (dónde x es una ciudad aleatoria).
+// Empleamos await para facilitar el manejo de código asíncrono y facilitar la lectura del código, ya q la alternativa con then 
+// queda menos legible.
+
+// Habria que mejorarlo para mejorar el rendimiento (tarda demasiado en generar las preguntas y respuestas)
+
+// Habria que mejorarlo para asegurarse que todas las ciudades generadas tienen disponibles los datos de poblacion, 
+// buscar otra ciudad si esa falla...
+
+// Habria que filtrar ciudades con un numero minimo de habitantes para que las ciudades sean conocidas y no pueblos perdidos 
+// que nadie conoce...
 
 class GeneradorDePreguntasDeCiudades {
 
     // Función para obtener una ciudad aleatoria
-    ciudadRandom() {
+    async ciudadRandom() {
         const consultaSparql = `
             SELECT ?ciudad ?ciudadLabel ?codigoQ
             WHERE {
@@ -22,11 +32,12 @@ class GeneradorDePreguntasDeCiudades {
             'Accept': 'application/json',
         };
     
-        return fetch(`${urlApiWikidata}?query=${encodeURIComponent(consultaSparql)}&format=json`, {
-            headers: headers,
-        })
-        .then(response => response.json())
-        .then(datos => {
+        try {
+            const response = await fetch(`${urlApiWikidata}?query=${encodeURIComponent(consultaSparql)}&format=json`, {
+                headers: headers,
+            });
+    
+            const datos = await response.json();
             const ciudades = datos.results.bindings;
     
             if (ciudades.length > 0) {
@@ -35,73 +46,85 @@ class GeneradorDePreguntasDeCiudades {
                 const codigoQ = ciudadAleatoria.ciudad.value.split('/').pop();
                 return [nombreCiudad, codigoQ];
             } else {
-                console.log('No se encontraron ciudades.');
                 return null;
             }
-        });
+        } catch (error) {
+            console.error(`Error al obtener ciudad aleatoria: ${error.message}`);
+            return null;
+        }
     }
 
     // Función para obtener la población de una ciudad
-    obtenerPoblacionCiudad(codigoCiudad) {
+    async obtenerPoblacionCiudad(codigoCiudad) {
         const url = `https://www.wikidata.org/w/api.php?action=wbgetclaims&format=json&entity=${codigoCiudad}&property=P1082`;
-        return fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            const populationClaim = data.claims.P1082[0].mainsnak.datavalue.value.amount;
-            return parseInt(populationClaim);
-        });
+    
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+    
+            if (data.claims && data.claims.P1082 && data.claims.P1082[0] && data.claims.P1082[0].mainsnak && data.claims.P1082[0].mainsnak.datavalue && data.claims.P1082[0].mainsnak.datavalue.value && data.claims.P1082[0].mainsnak.datavalue.value.amount) {
+                const populationClaim = data.claims.P1082[0].mainsnak.datavalue.value.amount;
+                return parseInt(populationClaim);
+            } else {
+                return null;
+            }
+        } catch (error) {
+            console.error(`Error al obtener población: ${error.message}`);
+            return null;
+        }
     }
 
     // Función para obtener la población de una ciudad aleatoria con reintentos
-    poblacionCiudadAleatoria() {
-        const intentosMaximos = 25;
-        let intentos = 0;
-    
-        const intentarObtenerPoblacion = () => {
-            return this.ciudadRandom()
-            .then(([nombreCiudad, codigoCiudad]) => {
-                return this.obtenerPoblacionCiudad(codigoCiudad);
-            })
-            .catch(error => {
-                intentos += 1;
-                if (intentos < intentosMaximos) {
-                    return intentarObtenerPoblacion();
-                } else {
-                    throw new Error('No se pudo obtener la población después de varios intentos.');
-                }
-            });
-        };
-    
-        return intentarObtenerPoblacion();
+    async poblacionCiudadAleatoria() {
+        const [nombreCiudad, codigoCiudad] = await this.ciudadRandom();
+        const poblacion = await this.obtenerPoblacionCiudad(codigoCiudad);
+        return poblacion;
     }
 
     // Función principal
-    main() {
-        return this.ciudadRandom()
-        .then(([nombreCiudad, codigoQ]) => {
-            return this.obtenerPoblacionCiudad(codigoQ)
-            .then(poblacion => {
+    async ejecutarGenerador() {
+        try {
+            const [nombreCiudad, codigoQ] = await this.ciudadRandom();
+            const poblacion = await this.obtenerPoblacionCiudad(codigoQ);
+
+            if (poblacion !== null) {
                 const pregunta = `¿Cuál es la población de ${nombreCiudad.charAt(0).toUpperCase() + nombreCiudad.slice(1)}?`;
-                const respuestaReal = `SOLUCION: La población de ${nombreCiudad.charAt(0).toUpperCase() + nombreCiudad.slice(1)} es ${poblacion.toLocaleString()} habitantes.`;
-    
-                return this.poblacionCiudadAleatoria()
-                .then(respuestaA => {
-                    const respuestaB = this.poblacionCiudadAleatoria();
-                    const respuestaC = this.poblacionCiudadAleatoria();
-                    const respuestaD = poblacion;
-    
-                    console.log(pregunta);
-                    console.log(respuestaReal);
-                    console.log("a)" + respuestaA);
-                    console.log("b)" + respuestaB);
-                    console.log("c)" + respuestaC);
-                    console.log("d)" + respuestaD);
-                });
-            });
-        });
+                const respuestaReal = `SOLUCION: La población de ${nombreCiudad.charAt(0).toUpperCase() + nombreCiudad.slice(1)} es ${poblacion.toLocaleString()}.`;
+
+                // Promise devuelve un array un array de los resultados de todas las promesas, en el mismo orden en que se 
+                // proporcionaron, una vez que todas las promesas en el iterable se han resuelto
+                const respuestasAleatorias = await Promise.all([
+                    this.poblacionCiudadAleatoria(),
+                    this.poblacionCiudadAleatoria(),
+                    this.poblacionCiudadAleatoria(),
+                ]);
+
+                const [respuestaA, respuestaB, respuestaC] = respuestasAleatorias;
+
+                if (respuestaA !== null && respuestaB !== null && respuestaC !== null) {
+                    this.mostrarPreguntasRespuestas(pregunta, respuestaReal, respuestaA, respuestaB, respuestaC, poblacion);
+                } else {
+                    await this.ejecutarGenerador();
+                }
+            } else {
+                await this.ejecutarGenerador();
+            }
+        } catch (error) {
+            console.error(error);
+        }
     }
 
+    // Función para imprimir por pantalla las preguntas y las respuestas
+    mostrarPreguntasRespuestas(pregunta, respuestaReal, respuestaA, respuestaB, respuestaC, respuestaD) {
+        console.log(pregunta);
+        console.log(respuestaReal);
+        console.log("a)" + respuestaA);
+        console.log("b)" + respuestaB);
+        console.log("c)" + respuestaC);
+        console.log("d)" + respuestaD);
+    }
 }
 
+// Ejemplo de ejecución
 var generadorDePreguntas = new GeneradorDePreguntasDeCiudades();
-generadorDePreguntas.main();
+generadorDePreguntas.ejecutarGenerador();
