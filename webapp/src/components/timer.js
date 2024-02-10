c513a4b35638046449c03f6fcc9a9dbef587cbc8
uo288543 <uo288543@uniovi.es>
2024-02-10 13:48:41 +0100

    Added button to hide or show

--- webapp/src/components/timer.js
+++ webapp/src/components/timer.js
@@ -4,6 +4,7 @@ import "./css/timer.css"
 
 const Clock = () => {
     const[count, setCount] = useState(15)
+    const [showClock, setShowClock] = useState(true);
 
     useEffect(() => {
         const interval = setInterval(() => {
@@ -18,29 +19,43 @@ const Clock = () => {
         setCount(15);
     };
 
+    const toggleClock = () => {
+        setShowClock(!showClock);
+    };
+
     return (
         <div>
-            <h1>Reloj</h1>
-            <p><i className="material-icons">access_time</i>{count}</p>
-            <button onClick={handleReset}>Reiniciar contador</button>
-            <CountdownCircleTimer
+          <button onClick={toggleClock}>Mostrar/ocultar reloj</button>
+          {showClock && (
+            <div>
+              <h1>Reloj</h1>
+              <p>
+                <i className="material-icons">access_time</i>
+                {count}
+              </p>
+              <button onClick={handleReset}>Reiniciar contador</button>
+              <CountdownCircleTimer
                 isPlaying
                 duration={15}
                 colors={["#0bfc03", "#F7B801", "#f50707", "#A30000"]}
                 colorsTime={[10, 6, 3, 0]}
                 onComplete={() => ({ shouldRepeat: true, delay: 1 })}
-            >
+              >
                 {({ remainingTime }) => {
-                return (
-                <div class="timer">
-                    <div className="value"><span>{remainingTime}</span></div>
-                    <div className="text">seconds</div> 
-                </div>
-                )
-            }}
-            </CountdownCircleTimer>
+                  return (
+                    <div className="timer">
+                      <div className="value">
+                        <span>{remainingTime}</span>
+                      </div>
+                      <div className="text">seconds</div>
+                    </div>
+                  );
+                }}
+              </CountdownCircleTimer>
+            </div>
+          )}
         </div>
-    );
-}
+      );
+    };
 
 export default Clock;
