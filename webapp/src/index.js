bce677d4c97ed3c13c6405b4675882ab3104ad32
uo288543 <uo288543@uniovi.es>
2024-02-14 17:20:02 +0100

    Added routing and error page

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -4,11 +4,34 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Clock from './components/timer';
+import ErrorPage from "./components/errorPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/test",
+    element: <Clock />,
+  },
+]);
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    {/*<App />*/}
+    <RouterProvider router={router}/>
+</React.StrictMode>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
