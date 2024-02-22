f928913c6cce9681d1dbdada8e49df47bd03e1ce
uo288245 <uo288245@uniovi.es>
2024-02-22 17:53:48 +0100

    Added first navbar prototype

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -5,6 +5,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
+import NavBar from './components/NavBar';
 
 function App() {
   const [showLogin, setShowLogin] = useState(true);
@@ -14,24 +15,27 @@ function App() {
   };
 
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <Typography component="h1" variant="h5" align="center" sx={{ marginTop: 2 }}>
-        Welcome to the 2024 edition of the Software Architecture course
-      </Typography>
-      {showLogin ? <Login /> : <AddUser />}
-      <Typography component="div" align="center" sx={{ marginTop: 2 }}>
-        {showLogin ? (
-          <Link name="gotoregister" component="button" variant="body2" onClick={handleToggleView}>
-            Don't have an account? Register here.
-          </Link>
-        ) : (
-          <Link component="button" variant="body2" onClick={handleToggleView}>
-            Already have an account? Login here.
-          </Link>
-        )}
-      </Typography>
-    </Container>
+    <div>
+      <NavBar/>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Typography component="h1" variant="h5" align="center" sx={{ marginTop: 2 }}>
+          Welcome to the 2024 edition of the Software Architecture course
+        </Typography>
+        {showLogin ? <Login /> : <AddUser />}
+        <Typography component="div" align="center" sx={{ marginTop: 2 }}>
+          {showLogin ? (
+            <Link name="gotoregister" component="button" variant="body2" onClick={handleToggleView}>
+              Don't have an account? Register here.
+            </Link>
+          ) : (
+            <Link component="button" variant="body2" onClick={handleToggleView}>
+              Already have an account? Login here.
+            </Link>
+          )}
+        </Typography>
+      </Container>
+    </div>
   );
 }
 
