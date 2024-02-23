38d7232e83b4b87fde30670413a0c871f69b7509
samuel <uo289689@uniovi.es>
2024-02-23 18:58:31 +0100

    Fix of the userSchema of user-model.js

--- users/userservice/user-model.js
+++ users/userservice/user-model.js
@@ -1,35 +1,40 @@
 const mysql = require('mysql2/promise');
 
-// Crear un pool de conexiones a la base de datos()
+// Create a connection pool to the database
 const pool = mysql.createPool({
     host: process.env.REACT_APP_DB_HOST,
     port: process.env.REACT_APP_DB_PORT,
     user: process.env.REACT_APP_DB_USER,
     password: process.env.REACT_APP_DB_PASSWORD,
     database: process.env.REACT_APP_DB_NAME,
-    waitForConnections: true, //determina si las conexiones se encolaran o se rechazaran si el pool ha llegado al límite
-    connectionLimit: 10, // límite máximo de conexiones simultáneas
-    queueLimit: 0 // límite máximo de conexiones en la cola de espera
+    waitForConnections: true, // determines if connections should be queued or rejected if the pool has reached its limit
+    connectionLimit: 10, // maximum limit of simultaneous connections
+    queueLimit: 0 // maximum limit of connections in the waiting queue
 });
 
-// Definir el esquema del usuario
+// Define the user schema
 const userSchema = `
     CREATE TABLE IF NOT EXISTS users (
         id INT AUTO_INCREMENT PRIMARY KEY,
-        username VARCHAR(255) NOT NULL,
+        username VARCHAR(255) UNIQUE NOT NULL,
         password VARCHAR(255) NOT NULL,
-        createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        name VARCHAR(255) NOT NULL,
+        surname VARCHAR(255) NOT NULL,
+        createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        imageUrl VARCHAR(255),
+        score INT DEFAULT 0,
+        correctly_answered questions INT DEFAULT 0,
+        incorrectly_answered questions INT DEFAULT 0,
+        total_time_played INT DEFAULT 0,
+        number_of_games played INT DEFAULT 0
     )
 `;
 
-// Crear la tabla de usuarios si no existe
+// Create the users table if it doesn't exist
 pool.query(userSchema)
-    .then(() => {
-        console.log('Tabla de usuarios creada o ya existe');
-    })
     .catch((err) => {
-        console.error('Error al crear la tabla de usuarios:', err);
+        console.error('Error creating users table:', err);
     });
 
-// Exportar el pool de conexiones para su uso en otros archivos
+// Export the connection pool for use in other files
 module.exports = pool;
