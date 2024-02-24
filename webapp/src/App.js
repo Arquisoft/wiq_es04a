f561d6fd94844c9f25566f784f5a7fb60cdeba41
Pablo Barrero Cruz <uo289642@uniovi.es>
2024-02-25 00:24:01 +0100

    Created the dynamic part of the instructions section
    I have completed the instructions section by adding the dynamic part that was missing. Still, it remains to look for photos for the games and review the description text

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,7 +1,6 @@
 import React, { Fragment } from 'react';
 import AddUser from './pages/AddUser';
 import Instructions from './pages/Instructions';
-import GameInstruction from './pages/GameInstuction';
 import Login from './pages/Login';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
@@ -18,7 +17,6 @@ function App() {
           <Route path="/login" element={<Login />}/>
           <Route path="/register" element={<AddUser />}/>
           <Route path="/instructions" element={<Instructions />}/>
-          <Route path="/instructions/gameinstruction" element={<GameInstruction />}/>
         </Routes>
       <Footer/>
     </Fragment>
--- webapp/src/pages/Instructions.js
+++ webapp/src/pages/Instructions.js
@@ -2,9 +2,42 @@ import * as React from 'react';
 import { Container } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 import {Button, Typography, Box, Grid } from '@mui/material';
-import {Link} from 'react-router-dom';
+import { useState } from 'react';
+import datos from '../data/gameInfo.json';
+
+
+
+const GameInfo = ({ informacion }) => {
+    console.log('El componente se ha montado');
+    return (
+        <Box width='100%' sx={{marginBottom:20}} >
+            {informacion}
+        </Box>
+    );
+  };
 
 const Instructions = () => {
+
+    const [info, setInfo] = useState(null);
+
+    const newGameInfo = (key) => {
+        console.log('El componente se ha montado');
+
+
+
+        setInfo(
+
+            <Box sx={{  marginTop: 10, display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', width:'100%'}}>
+                <Box sx={{width: '30%', display: 'flex', flexDirection: 'column', marginTop: '10'}}>
+                    <img src={datos[key].foto} alt="Foto del minijuego" style={{ maxWidth: '100%', height: 'auto', border: '2px solid #006699', borderRadius: '5px'}}/>
+                </Box>
+                <Box sx={{width: '40%', display: 'flex', flexDirection: 'column', marginLeft: 10}}>
+                    <Typography component="h1" variant="h3" align="center" sx={{marginBotton: 10, fontWeight: 'bold'}}> {datos[key].nombre} </Typography>
+                    <Typography  component="body" variant="body1" align="center" sx={{ textAlign: 'justify', marginTop: 6}}> {datos[key].descripcion} </Typography>
+                </Box>
+            </Box>
+        );
+     };
     return (
 
 
@@ -24,13 +57,13 @@ const Instructions = () => {
                 Additionally, for the more daring, we have prepared alternative game modes to the original so they can further hone their skills.
             </Typography>
 
-            <Box width="100%">
-                <Box display="flex" width="100%" justifyContent="center">
+            <Box width="100%" sx={{ marginBottom: 3}}>
+                <Box display="flex" width="100%" justifyContent="center"  >
                     <Grid  container spacing={2} sx={{ width: '80%', marginTop: 3 }}>
 
                         <Grid item xs={6} sm={3} key={"Sages"} sx={{ marginTop: 3}}>
                             <Box textAlign="center">
-                                <Button width="100%" component={Link} to="/GameInstruction" color="primary" disabled={false} size="large" variant="outlined" sx={{ width: '100%', height: '100px'  }} >
+                                <Button width="100%" color="primary" disabled={false} size="large" variant="outlined" sx={{ width: '100%', height: '100px'  }} onClick={() => newGameInfo([0])}>
                                     Wise Men Stack
                                 </Button>
                             </Box>
@@ -38,7 +71,7 @@ const Instructions = () => {
 
                         <Grid item xs={6} sm={3} key={"Discarding"} sx={{ marginTop: 3}}>
                             <Box textAlign="center">
-                                <Button width="100%" component={Link} to="/GameInstruction" color="primary" disabled={false} size="large" variant="outlined" sx={{ width: '100%', height: '100px'  }}>
+                                <Button width="100%" color="primary" disabled={false} size="large" variant="outlined" sx={{ width: '100%', height: '100px'  }} onClick={() => newGameInfo([1])}>
                                     Discarding
                                 </Button>
                             </Box>
@@ -46,7 +79,7 @@ const Instructions = () => {
 
                         <Grid item xs={6} sm={3} key={"HotQuestion"} sx={{ marginTop: 3}}>
                             <Box textAlign="center">
-                                <Button width="100%" component={Link} to="/GameInstruction" color="primary" disabled={false} size="large" variant="outlined" sx={{ width: '100%', height: '100px'  }}>
+                                <Button width="100%" color="primary" disabled={false} size="large" variant="outlined" sx={{ width: '100%', height: '100px'  }} onClick={() => newGameInfo([2])}>
                                      Warm Question
                                 </Button>
                             </Box>
@@ -54,7 +87,7 @@ const Instructions = () => {
 
                         <Grid item xs={6} sm={3} key={"Cities"} sx={{ marginTop: 3}}>
                             <Box textAlign="center">
-                                <Button width="100%" component={Link} to="/GameInstruction" color="primary" disabled={false} size="large" variant="outlined" sx={{ width: '100%', height: '100px'  }}>
+                                <Button width="100%" color="primary" disabled={false} size="large" variant="outlined" sx={{ width: '100%', height: '100px'  }} onClick={() => newGameInfo([3])}>
                                     Discovering Cities
                                 </Button>
                             </Box>
@@ -68,7 +101,7 @@ const Instructions = () => {
 
                         <Grid item xs={6} sm={3} key={"LastCall"} sx={{ marginTop: 3}}>
                             <Box textAlign="center">
-                                <Button  width="100%" component={Link} to="/GameInstruction" color="primary" disabled={false} size="large" variant="outlined" sx={{ width: '100%', height: '100px'  }}>
+                                <Button  width="100%" color="primary" disabled={false} size="large" variant="outlined" sx={{ width: '100%', height: '100px'  }} onClick={() => newGameInfo([4])}>
                                     Last call
                                 </Button>
                             </Box>
@@ -76,7 +109,7 @@ const Instructions = () => {
 
                         <Grid item xs={6} sm={3} key={"Calculator"} sx={{ marginTop: 3}}>
                             <Box textAlign="center">
-                                <Button width="100%" component={Link} to="/GameInstruction" color="primary" disabled={false} size="large" variant="outlined" sx={{ width: '100%', height: '100px'  }}>
+                                <Button width="100%" color="primary" disabled={false} size="large" variant="outlined" sx={{ width: '100%', height: '100px'  }} onClick={() => newGameInfo([5])}>
                                     The human calculator
                                 </Button>
                             </Box>
@@ -84,7 +117,7 @@ const Instructions = () => {
 
                         <Grid item xs={6} sm={3} key={"POH"} sx={{ marginTop: 3}}>
                             <Box textAlign="center">
-                                <Button width="100%" component={Link} to="/GameInstruction" color="primary" disabled={false} size="large" variant="outlined" sx={{ width: '100%' , height: '100px' }}>
+                                <Button width="100%" color="primary" disabled={false} size="large" variant="outlined" sx={{ width: '100%' , height: '100px' }} onClick={() => newGameInfo([6])}>
                                     The part <br/>for the whole
                                 </Button>
                             </Box>
@@ -92,7 +125,7 @@ const Instructions = () => {
 
                         <Grid item xs={6} sm={3} key={"Challenge"} sx={{ marginTop: 3}}>
                             <Box textAlign="center">
-                                <Button width="100%" component={Link} to="/GameInstruction" color="primary" disabled={false} size="large" variant="outlined" sx={{ width: '100%' , height: '100px' }}>
+                                <Button width="100%" color="primary" disabled={false} size="large" variant="outlined" sx={{ width: '100%' , height: '100px' }} onClick={() => newGameInfo([7])}>
                                     The challenge
                                 </Button>
                             </Box>
@@ -100,6 +133,7 @@ const Instructions = () => {
                     </Grid>
                 </Box>
             </Box>
+            <GameInfo informacion={info}/>
         </Container>
     );
 };
