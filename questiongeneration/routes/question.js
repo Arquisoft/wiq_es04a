caffdf3d901f7cf1fbe25dd1baf65ff2b27267bc
Pablo <uo265314@uniovi.es>
2024-02-25 23:53:42 +0100

    Refactored express service for the question generation to follow standart directroy structure keeping the same functionality

---
+++ questiongeneration/routes/question.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const router = express.Router();
+const { getRandomCity, getCityPopulation, getPopulationFromRandomCity, shuffleArray } = require('../utils/cityPopulation');
+
+// Manejo de la ruta '/question'
+router.get('/', async (_req, res) => {
+    try {
+        const questions = [];
+        
+        for (let i = 0; i < 5; i++) {
+            const [nombreCiudad, codigoQ] = await getRandomCity();
+            const poblacion = await getCityPopulation(codigoQ);
+    
+            if (poblacion !== null) {
+                const questionText = `¿Cuál es la población de ${nombreCiudad.charAt(0).toUpperCase() + nombreCiudad.slice(1)}?`;
+                const correctAnswer = poblacion;
+    
+                // Promise devuelve un array de los resultados de todas las promesas
+                let options = await Promise.all([
+                    getPopulationFromRandomCity(),
+                    getPopulationFromRandomCity(),
+                    getPopulationFromRandomCity(),
+                ]);
+    
+                // Filtrar los elementos null y llamar a poblacionAleatoria para reemplazarlos
+                while (options.some(respuesta => respuesta === null)) {
+                    options = await Promise.all(
+                        options.map(async (respuesta) => {
+                            if (respuesta === null) {
+                                return await getPopulationFromRandomCity();
+                            } else {
+                                return respuesta;
+                            }
+                        })
+                    );
+                }
+    
+                options.push(correctAnswer);
+    
+                // Desordenar las opciones
+                const shuffledOptions = shuffleArray(options);
+    
+                // Crear el objeto de pregunta
+                const newQuestion = {
+                    id: i,
+                    question: questionText,
+                    options: shuffledOptions,
+                    correctAnswer: correctAnswer,
+                };
+    
+                questions.push(newQuestion);
+            }
+        }
+    
+        res.json(questions);
+    
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error al procesar la solicitud' });
+    }
+});
+
+module.exports = router;
