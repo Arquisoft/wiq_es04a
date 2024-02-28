ecc998a0825fe206ae7552003e5ec0851cd9f63c
Pablo <uo265314@uniovi.es>
2024-02-28 19:18:59 +0100

    Refactored Wikidata access to include it in its own service and actualized imports in router file.

--- questions/routes/question.js
+++ questions/routes/question.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const dbService = require('../services/question-data-service');
-const { readFromFile, getRandomEntity, getProperties, shuffleArray } = require('../utils/generalQuestions');
-const { db } = require('../services/question-data-model');
+const utils = require('../utils/generalQuestions');
+const wikidataService = require('../services/wikidata-service');
 
 // Manejo de la ruta '/question'
 router.get('/', async (_req, res) => {
     try {
-        const json = await readFromFile("../questions/utils/question.json");
+        const json = await utils.readFromFile("../questions/utils/question.json");
 
         const questions = [];
-        var entity = json[0].city;
+        var entity = json[0];
         //entity = json[1].football;
       
         for (let i = 0; i < 5; i++) {
@@ -23,7 +23,7 @@ router.get('/', async (_req, res) => {
             var question = entity.properties[pos].template;
             var category = entity.properties[pos].category[0];
 
-            var [entityName, searched_property] = await getRandomEntity(instance, property);
+            var [entityName, searched_property] = await wikidataService.getRandomEntity(instance, property);
     
             if (searched_property !== null) {
                 //const questionText = question + entityName.charAt(0).toUpperCase() + entityName.slice(1) +`?`;
@@ -32,11 +32,11 @@ router.get('/', async (_req, res) => {
                 const correctAnswer = searched_property;
     
                 // options will contain 3 wrong answers plus the correct one
-                const options = await getProperties(property);
+                const options = await wikidataService.getProperties(property);
                 options.push(correctAnswer);
 
                 // Shuffle options, we will not know where is the correct option
-                const shuffledOptions = shuffleArray(options);
+                const shuffledOptions = utils.shuffleArray(options);
   
                 // Create object with data for the question
                 const newQuestion = {
--- questions/utils/generalQuestions.js
+++ questions/utils/generalQuestions.js
@@ -1,4 +1,3 @@
-const axios = require('axios');
 const fs = require('fs').promises;
 
 async function readFromFile(filePath) {
@@ -7,85 +6,6 @@ async function readFromFile(filePath) {
         return JSON.parse(jsonData);
     } catch (error) {
         console.error(`Error al leer el archivo JSON: ${error.message}`);
-        return null;
-    }
-}
-
-
-async function getRandomEntity(instance, property) {
-    const consultaSparql = `
-        SELECT ?entity ?entityLabel ?property
-        WHERE {
-            ?entity wdt:P31 wd:${instance};   
-                wdt:${property} ?property.   
-            ?entity rdfs:label ?entityLabel.  
-            FILTER(LANG(?entityLabel) = "es")
-    }
-    `;
-    // it is better to use the FILTER rather than SERVICE
-    //SERVICE wikibase:label { bd:serviceParam wikibase:language "es". }
-  
-    const urlApiWikidata = 'https://query.wikidata.org/sparql';
-    const headers = {
-        'User-Agent': 'EjemploCiudades/1.0',
-        'Accept': 'application/json',
-    };
-  
-    try {
-        response = await axios.get(urlApiWikidata, {
-            params: {
-              query: consultaSparql,
-              format: 'json' // Debe ser una cadena
-            },
-            headers: headers,
-          });
-  
-        const data = await response.data
-        const entities = data.results.bindings;
-  
-        if (entities.length > 0) {
-            const randomEntity = entities[Math.floor(Math.random() * entities.length)];
-            const entityName = randomEntity.entityLabel.value;
-            const property = randomEntity.property.value;
-            return [entityName, property];
-        } else {
-            return null;
-        }
-
-    } catch (error) {
-        console.error(`Error al obtener entidad aleatoria: ${error.message}`);
-        return null;
-    }
-}
-
-
-async function getProperties(property) {
-    const consultaSparql = `
-        SELECT ?property
-        WHERE {
-            ?entity wdt:${property} ?property.   
-        }
-    `;
-    const urlApiWikidata = 'https://query.wikidata.org/sparql';
-    try {
-        response = await axios.get(urlApiWikidata, {
-            params: {
-              query: consultaSparql,
-              format: 'json' // Debe ser una cadena
-            }});
-        const data = await response.data;
-        const list = data.results.bindings;
-
-        if(list.length>0) {
-            const properties = new Array(3);
-            for(var i = 0; i < 3 ; i++) {
-                properties[i] = list[Math.floor(Math.random() * list.length)].property.value;
-            }
-            return properties;
-        }
-        return null;
-    } catch (error) {
-        console.error(`Error al obtener propiedades: ${error.message}`);
         return null;
     }
 }
@@ -101,7 +21,5 @@ function shuffleArray(array) {
 
 module.exports = {
     readFromFile,
-    getRandomEntity,
-    getProperties,
     shuffleArray
 };
