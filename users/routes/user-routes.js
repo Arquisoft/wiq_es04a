26aedc21a8a1e78fc4640934ab9441c9c398eee5
samuel <uo289689@uniovi.es>
2024-03-01 17:28:27 +0100

    Established route for editing a user

--- users/routes/user-routes.js
+++ users/routes/user-routes.js
@@ -6,6 +6,7 @@ const { User } = require('../models/user-model');
 //User internal routes
 const apiRoutes = require('../services/user-api');
 
+// Route for add a user
 router.post('/add', async (req, res) => {
     try {
         const { username, password, name, surname, imageUrl } = req.body;
@@ -48,6 +49,45 @@ router.post('/add', async (req, res) => {
     }
 });
 
+// Route for edit a user
+router.post('/edit', async (req, res) => {
+    try {
+        const { username, total_score, correctly_answered_questions, incorrectly_answered_questions, total_time_played, games_played } = req.body;
+
+        // Find the user in the database by their username
+        const userToUpdate = await User.findOne({
+            where: {
+                username: username
+            }
+        });
+
+        // Check if the user exists
+        if (!userToUpdate) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        // Update the user's fields with the provided values
+        userToUpdate.total_score = total_score;
+        userToUpdate.correctly_answered_questions = correctly_answered_questions;
+        userToUpdate.incorrectly_answered_questions = incorrectly_answered_questions;
+        userToUpdate.total_time_played = total_time_played;
+        userToUpdate.games_played = games_played;
+
+        // Save the changes to the database
+        await userToUpdate.save();
+
+        res.json({ message: 'User updated successfully' });
+    } catch (error) {
+        if (error.name === 'SequelizeValidationError') {
+            // Validation errors
+            const validationErrors = error.errors.map(err => err.message);
+            res.status(400).json({ error: 'Validation error', details: validationErrors });
+        } else {
+            // Other errors
+            res.status(400).json({ error: error.message });
+        }
+    }
+});
 
 //Api middleware
 router.use('/api', apiRoutes);
