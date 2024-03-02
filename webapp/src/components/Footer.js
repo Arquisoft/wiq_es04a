d544e947a9be8cdc7dd75c40034ecd8a253d9632
Pablo <uo265314@uniovi.es>
2024-03-02 16:33:35 +0100

    Instructions being responsive and show/hide games. Solved footer problem with React ShowLabel prop

--- webapp/src/components/Footer.js
+++ webapp/src/components/Footer.js
@@ -1,17 +1,15 @@
 import * as React from 'react';
-import { BottomNavigation, Toolbar, Typography, useTheme } from '@mui/material';
+import { AppBar, Toolbar, Typography } from '@mui/material';
 
 const Footer = () => {
-    const theme = useTheme();
-
     return (
-      <BottomNavigation position="static" sx={{ backgroundColor: theme.palette.primary.main, color: theme.palette.primary.contrastText, bottom: 0, left: 0, width: '100%', zIndex: 1000 }}>
+      <AppBar component="footer" position="static" sx={{ backgroundColor: "primary", color: "white", bottom: 0, left: 0, width: '100%', zIndex: 1000 }}>
         <Toolbar>
           <Typography sx={{ margin: 'auto' }}>
             © WIQ_ES04A
           </Typography>
         </Toolbar>
-      </BottomNavigation>
+      </AppBar>
     );
 };
 
