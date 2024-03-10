621d19f441eb176e089cb868873af2de1770884e
Pablo <uo265314@uniovi.es>
2024-03-10 13:17:36 +0100

    2nd try to fix network issue in Game

--- webapp/src/pages/Game.js
+++ webapp/src/pages/Game.js
@@ -8,7 +8,9 @@ import { useNavigate } from 'react-router-dom';
 import { SessionContext } from '../SessionContext';
 import { useContext } from 'react';
 
-const apiEndpoint = process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000';
+// const apiEndpoint = process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000';
+const apiEndpoint = 'http://gatewayservice:8000';
+
 
 const Game = () => {
     const navigate = useNavigate();
