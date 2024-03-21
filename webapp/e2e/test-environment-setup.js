5b6e18bcd247fa4bb55aae6db950b1acc8f91491
uo288543 <uo288543@uniovi.es>
2024-03-21 12:21:51 +0100

    Actualizacion del enviroment y dependencias

--- webapp/e2e/test-environment-setup.js
+++ webapp/e2e/test-environment-setup.js
@@ -1,1 +1,34 @@
+////const { MongoMemoryServer } = require('mongodb-memory-server');
+const userMemoryDatabase = require('mysql2-promise')();
 
+//let mongoserver;
+let userservice;
+let gatewayservice;
+let con;
+
+async function startServer() {
+    //console.log('Starting MongoDB memory server...');
+    //mongoserver = await MongoMemoryServer.create();
+    //const mongoUri = mongoserver.getUri();
+    //process.env.MONGODB_URI = mongoUri;
+
+    const mariadbConfig = {
+        host: 'localhost',
+        user: 'root',
+        database: 'base_de_datos_de_usuarios',
+        memory: true
+    };
+    //con = await userMemoryDatabase.createConnection(mariadbConfig);
+
+    userMemoryDatabase.configure({
+        "host": "mariadb",
+        "user": "foo",
+        "password": "bar",
+        "database": "base_de_datos_de_usuarios"
+    });
+
+    userservice = await require("../../users/index");
+    gatewayservice = await require("../../gatewayservice/gateway-service");
+  }
+
+  startServer();
