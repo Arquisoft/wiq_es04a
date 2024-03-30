6061e8f47e133224545fedc3ebbd28340e0dfc70
teresagg <uo288347@uniovi.es>
2024-03-30 17:08:09 +0100

    Fixed question api doc link

--- webapp/src/components/Footer.js
+++ webapp/src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
         sx={{ backgroundColor: "primary", color: "white", bottom: 0, left: 0, width: '100%', zIndex: 1000}}>
         <Toolbar>
         <Typography sx={{ margin: 'auto' }}>
-            <Link href='https://app.swaggerhub.com/apis/UO288347_1/questions-api/1.0.0' target="_blank" color="inherit">Questions API doc</Link>
+            <Link href='https://app.swaggerhub.com/apis-docs/UO288347_1/questions-api/1.0.0' target="_blank" color="inherit">Questions API doc</Link>
           </Typography>
           <Typography sx={{ margin: 'auto' }}>
             <Link href="https://github.com/Arquisoft/wiq_es04a" target="_blank" rel="noopener" color="inherit">
