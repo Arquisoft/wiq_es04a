f7ad0dd91477dbe753dc41f63232ce5d9577960b
teresagg <uo288347@uniovi.es>
2024-03-30 10:55:32 +0100

    Fixed problem with questions api doc

--- webapp/src/components/Footer.js
+++ webapp/src/components/Footer.js
@@ -3,10 +3,13 @@ import { AppBar, Toolbar, Typography, Link } from '@mui/material';
 
 const Footer = () => {
     return (
-      <AppBar component="footer" position="static" sx={{ backgroundColor: "primary", color: "white", bottom: 0, left: 0, width: '100%', zIndex: 1000 }}>
+      <AppBar component="footer" position="static" 
+        sx={{ backgroundColor: "primary", color: "white", bottom: 0, left: 0, width: '100%', zIndex: 1000}}>
         <Toolbar>
+        <Typography sx={{ margin: 'auto' }}>
+            <Link href='https://app.swaggerhub.com/apis-docs/uo288347/questions-openapi/0.1.0' target="_blank" color="inherit">Questions API doc</Link>
+          </Typography>
           <Typography sx={{ margin: 'auto' }}>
-            <Link href='/questionsapi' target="_blank" color="inherit">Questions API doc</Link>
             <Link href="https://github.com/Arquisoft/wiq_es04a" target="_blank" rel="noopener" color="inherit">
               © WIQ-ES04A
             </Link>
