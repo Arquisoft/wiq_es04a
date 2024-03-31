51e39c807965a3dd50abe20670e790c6be3a2585
teresagg <uo288347@uniovi.es>
2024-03-31 13:01:59 +0200

    Testing questions routes with mock object

--- questions/__tests/routes/question-routes.test.js
+++ questions/__tests/routes/question-routes.test.js
@@ -1,11 +1,11 @@
-const Question = require('../../services/question-data-model.js');
-//const { MongoMemoryServer } = require('mongodb-memory-server');
-const request = require('supertest');
-const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
 const questionRoutes = require('../../routes/question-routes.js');
-const questionFunctions = require('../../services/question-data-service');
+const axios = require('axios');
+const MockAdapter = require('axios-mock-adapter');
+
+const mockAxios = new MockAdapter(axios);
+
 
 //let mongoServer;
 let app = express();
@@ -28,79 +28,65 @@ const questionData2 = {
   language: "en"
 };
 
-
-async function addQuestion(questionData) {
-  const newQuestion = new Question({
-    question: questionData.question,
-    options: questionData.options,
-    correctAnswer: questionData.correctAnswer,
-    categories: questionData.categories,
-    language: questionData.language
-  });
-  //await newQuestion.save();
-  await questionFunctions.addQuestion(newQuestion);
-}
-
-beforeAll(async () => {
-   await mongoose.disconnect();
-    // Conectar a la base de datos de prueba
-    const uri = process.env.TEST_DATABASE_URI || 'mongodb://localhost:27017/test';
-    await mongoose.connect(uri);
-    // Limpiar la colección de preguntas antes de cada prueba
-    await Question.deleteMany({});
-});
-
-beforeEach(async () => {
-  //Load database with initial conditions
-  //await mongoose.connection.dropDatabase();
-  await Question.deleteMany({});
-  for(var i = 0; i < 10; i++) {
-    await addQuestion(questionData1);
-  }
-});
-
-  afterAll(async () => {
-    // Disconnect testing database after all tests
-    await mongoose.disconnect();
-    //await mongoServer.stop();
-  });
-
   describe('Question routes', function() {
-    it('It should get a question from the database', async function() {
-        const response = await request(app).get('/questions/');
+    it('It should get a question', async function() {
+        mockAxios.onGet('http://localhost:8000/questions').reply(200, questionData1);
+  
+        const response = await axios.get('http://localhost:8000/questions');
         await expect(response.status).toBe(200);
-        await expect(response.body.question).toBe('Which is the capital of Spain?');
+        await expect(response.data.question).toBe('Which is the capital of Spain?');
     });
 
-    it('It should get n questions from the database', async function() {
-      const response = await request(app).get('/questions/getQuestionsFromDb/3');
+    it('It should get n questions', async function() {
+      var array = [questionData1, questionData2, questionData1];
+      mockAxios.onGet('/questions/getQuestionsFromDb/3').reply(200, array);
+  
+      const response = await axios.get('/questions/getQuestionsFromDb/3');
       await expect(response.status).toBe(200);
-      await expect(response.body[0].question).toBe('Which is the capital of Spain?');
-      await expect(response.body[1].question).toBe('Which is the capital of Spain?');
-      await expect(response.body[2].question).toBe('Which is the capital of Spain?');
+      await expect(response.data.length).toBe(3);
+      await expect(response.data[0].question).toBe('Which is the capital of Spain?');
+      await expect(response.data[1].question).toBe('Which is the capital of UK?');
+      await expect(response.data[2].question).toBe('Which is the capital of Spain?');
 
     });
 
-    it('It should not get n questions from the database', async function() {
-      await mongoose.connection.dropDatabase();
-      const response = await request(app).get('/questions/getQuestionsFromDb/2');
-      await expect(response.status).toBe(200);
-      await expect(response.body).toBe('');
+    it('It should not get n questions because parameter is incorrect', async function() {
+      mockAxios.onGet('/questions/getQuestionsFromDb/-1').reply(400, 'Parameter "n" must be > 0.');
+      try {
+        await axios.get('/questions/getQuestionsFromDb/-1');
+      } catch (error) {
+        // Verifica que la solicitud haya fallado con un código de estado 400
+        expect(error.response.status).toBe(400);
+        
+        // Verifica que la respuesta contenga el mensaje de error esperado
+        expect(error.response.data).toBe('Parameter "n" must be > 0.');
+      }
     });
-
+  
     it('It should get n questions of certain category from the database', async function() {
-      const response = await request(app).get('/questions/getQuestionsFromDb/2/Geography');
+      var array = [questionData1, questionData2];
+      mockAxios.onGet('/questions/getQuestionsFromDb/2/Geography').reply(200, array);
+
+      const response = await axios.get('/questions/getQuestionsFromDb/2/Geography');
       await expect(response.status).toBe(200);
-      await expect(response.body[0].categories[0]).toBe("Geography");
-      await expect(response.body[1].categories[0]).toBe("Geography");
-      await expect(response.body[0].question).toBe('Which is the capital of Spain?');
-      await expect(response.body[1].question).toBe('Which is the capital of Spain?');
+      await expect(response.data[0].categories[0]).toBe("Geography");
+      await expect(response.data[1].categories[0]).toBe("Geography");
+      await expect(response.data[0].question).toBe('Which is the capital of Spain?');
+      await expect(response.data[1].question).toBe('Which is the capital of UK?');
     });
 
+    it('It should not get n questions of certain category', async function() {
+      mockAxios.onGet('/questions/getQuestionsFromDb/-1/Geography').reply(400, 'Parameter "n" must be > 0.');
+      try {
+        await axios.get('/questions/getQuestionsFromDb/-1/Geography');
+      } catch (error) {
+        // Verifica que la solicitud haya fallado con un código de estado 400
+        expect(error.response.status).toBe(400);
+        
+        // Verifica que la respuesta contenga el mensaje de error esperado
+        expect(error.response.data).toBe('Parameter "n" must be > 0.');
+      }
+    });
+    
   });
 
-describe('MongoDB Connection', () => {
-    it('should connect to the MongoDB server in memory', async () => {
-        expect(mongoose.connection.readyState).toBe(1); // 1 means connected
-    });
-});
