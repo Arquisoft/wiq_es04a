7ee9fe12263faa6d57f43878faa4aea32a55beed
teresagg <uo288347@uniovi.es>
2024-03-31 17:53:55 +0200

    Testing error when generating questions

--- questions/__tests/services/question-generation-service.test.js
+++ questions/__tests/services/question-generation-service.test.js
@@ -1,15 +1,10 @@
-const request = require('supertest');
-const axios = require('axios');
 const generator = require('../../services/generate-questions-service');
-const MockAdapter = require('axios-mock-adapter');
 const dbService = require('../../services/question-data-service');
 const wikidataService = require('../../services/wikidata-service');
 const generalQuestions = require('../../utils/generalQuestions');
 
 jest.mock('../../utils/generalQuestions');
 jest.mock('../../services/wikidata-service');
-
-
 jest.mock('../../services/question-data-service', () => ({
     addQuestion: jest.fn(), // Mockear la función addQuestion para que no haga nada
 }));
@@ -62,4 +57,32 @@ describe('Question generation', function() {
         // Verifica que la función haya realizado las operaciones esperadas
         expect(dbService.addQuestion).toHaveBeenCalledTimes(1);
       });
+
+      it('should cause an error', async () => {
+        // Configura los mocks de las dependencias según sea necesario para tus pruebas
+        const questions = entity;
+        generalQuestions.readFromFile.mockResolvedValue(questions);
+
+        const entityName = 'Madrid';
+        const searched_property = 'P18';
+        wikidataService.getRandomEntity.mockResolvedValue([entityName, searched_property]);
+    
+        wikidataService.getProperties.mockResolvedValue(['Barcelona', 'Paris', 'London']);
+        wikidataService.convertUrlsToLabels.mockResolvedValue(['Barcelona', 'Paris', 'London']);
+
+        generalQuestions.shuffleArray.mockResolvedValue(['Barcelona', 'Paris', 'London','Madrid'])
+    
+        dbService.addQuestion.mockResolvedValue();
+
+        console.error = jest.fn();
+        // Llama a la función que deseas probar
+        await generator.generateQuestions(1);
+    
+        // Verifica que la función haya realizado las operaciones esperadas
+        expect(dbService.addQuestion).toHaveBeenCalledTimes(0);
+
+        // Verifica si console.error fue llamado con el mensaje de error específico
+    expect(console.error).toHaveBeenCalledWith("Error generating questions: ","Cannot read properties of undefined (reading 'properties')");
+      });
+
 });
--- questions/__tests/services/wikidata-service.test.js
+++ questions/__tests/services/wikidata-service.test.js
@@ -1,4 +1,3 @@
-const request = require('supertest');
 const axios = require('axios');
 const wikidataService = require('../../services/wikidata-service');
 const MockAdapter = require('axios-mock-adapter');
@@ -6,30 +5,6 @@ const MockAdapter = require('axios-mock-adapter');
 const mockAxios = new MockAdapter(axios);
 
 const urlApiWikidata = 'https://query.wikidata.org/sparql';
-const headers = {
-    'User-Agent': 'QuestionGeneration/1.0',
-    'Accept': 'application/json',
-};
-const consultaSparql = `
-        SELECT ?entity ?entityLabel ?property
-        WHERE {
-            ?entity wdt:P31 wd:Q6256;   
-                wdt:P1082 ?property.   
-            ?entity rdfs:label ?entityLabel.  
-            FILTER(LANG(?entityLabel) = "en")
-            FILTER(?property>1000000)
-    }
-    `;
-
-const consultaSparqlProperties = `
-    SELECT DISTINCT ?property
-    WHERE {
-        ?entity wdt:P31 ?property. 
-        ?entity rdfs:label ?entityLabel. 
-        FILTER(LANG(?entityLabel) = "en")
-    }
-    LIMIT 400
-`;
 
 const entity = {
     "name": "country",
@@ -51,6 +26,7 @@ const entity = {
         }
     ]
 };
+
 
 beforeEach(() => {
     mockAxios.reset();
@@ -98,13 +74,8 @@ describe('Get entity from wikidata', function() {
 });
 
 describe('Get properties from wikidata', function() {
-    /*it('It should get a list of properties', async function() {
-        mockAxios.onGet(urlApiWikidata, {
-            params: {
-              query: consultaSparqlProperties,
-              format: 'json' // Debe ser una cadena
-            }
-        }).reply(200, 
+    it('It should get a list of properties', async function() {
+        mockAxios.onGet(urlApiWikidata).reply(200, 
             { 
                 results: {
                 bindings: [{
@@ -119,7 +90,7 @@ describe('Get properties from wikidata', function() {
         await expect(response[0]).toBe('P31');
         await expect(response[1]).toBe('P31');
         await expect(response[2]).toBe('P31');
-      });*/
+      });
     it('It should fail when accessing wikidata getting properties', async function() {
         mockAxios.onGet(urlApiWikidata).reply(400,"Error not found");
 
