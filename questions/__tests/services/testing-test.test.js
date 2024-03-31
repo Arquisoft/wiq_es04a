abc20053065071c25dbc9290f751e661e2c09688
uo288543 <145331195+uo288543@users.noreply.github.com>
2024-03-31 13:10:29 +0200

    Update memory test

--- questions/__tests/services/testing-test.test.js
+++ questions/__tests/services/testing-test.test.js
@@ -1,16 +1,19 @@
 const assert = require('assert');
-const questionFunctions = require('../../services/question-data-service');
 const { MongoMemoryServer } = require('mongodb-memory-server');
 const mongoose = require('mongoose');
+const Question = require('../../services/question-data-model');
 
 let mongoServer;
+let questionFunctions; 
 
 describe('Question Functions', function() {
     beforeAll(async function() {
+      //await mongoose.disconnect();
       mongoServer = await MongoMemoryServer.create();
       const mongoURI = mongoServer.getUri();
       process.env.DATABASE_URI = mongoURI;
       await mongoose.connect(mongoURI);
+      questionFunctions = require('../../services/question-data-service');
     });
   
     afterEach(async function() {
@@ -24,7 +27,7 @@ describe('Question Functions', function() {
     });
   
     describe('addQuestion', function() {
-      it('It should add a question to de database', async function() {
+      it('It should add a question to the database', async function() {
           const questionData = {
               question: "Which is the capital of Spain?",
               options: ["Madrid", "Barcelona", "Paris", "London"],
