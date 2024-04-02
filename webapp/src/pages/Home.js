9d29cc358b1f9d7c57a3d4a42404bbce089c6c3e
Pablo Barrero Cruz <uo289642@uniovi.es>
2024-04-02 20:38:06 +0200

    Color correction on the home and homepage

--- webapp/src/pages/Home.js
+++ webapp/src/pages/Home.js
@@ -13,14 +13,15 @@ const Home = () => {
             fontSize:'1.5rem',
             fontFamily: 'Arial Black, sans-serif',
 
-            color: '#990000',
-            backgroundColor: 'transparent',
-            border: '2px solid #990000',
+            color: '#339966',
+              backgroundColor: 'transparent',
+            
+            border: '2px solid #339966',
             transition: 'background-color 0.3s ease',
 
             '&:hover': {
-              backgroundColor: '#990000',
-              color: '#fff',
+              backgroundColor: '#339966',
+            color: '#fff',
             }
         },
 
