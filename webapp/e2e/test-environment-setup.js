4092042810ad1e94d27aa4037a2bf229cef4ef20
teresagg <uo288347@uniovi.es>
2024-04-08 00:17:12 +0200

    Trying to fix e2e problem

--- webapp/e2e/test-environment-setup.js
+++ webapp/e2e/test-environment-setup.js
@@ -1,1 +1,18 @@
-//Not used
+const {sequelize} = require('../../services/user-model.js');
+
+let userservice;
+let gatewayservice;
+
+async function startServer() {
+    process.env.NODE_ENV === 'test'
+    await sequelize.authenticate();
+    await sequelize.sync({ force: true });
+
+    console.log('Starting MariaDB Connection...');
+    userservice = await require("../../users/userservice/user-service");
+    gatewayservice = await require("../../gatewayservice/gateway-service");
+
+    
+  }
+
+  startServer();
