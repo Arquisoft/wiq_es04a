c2e158eb6a84a40f7b77388b360bdbe4e50d520b
uo288543 <uo288543@uniovi.es>
2024-04-09 20:06:50 +0200

    Progress on view of nav

--- android-apk/wiq-infinite-quest/App.js
+++ android-apk/wiq-infinite-quest/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Platform, StatusBar } from 'react-native';
 import { WebView } from 'react-native-webview';
+import Constants from 'expo-constants';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: Platform.OS === 'android' ? Constants.statusBarHeight : 0,
   },
   webview: {
     flex: 1,
+    marginTop: Platform.OS === 'android' ? -Constants.statusBarHeight : 0, 
+    paddingTop: 0
   },
 });
 
