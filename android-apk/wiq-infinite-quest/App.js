f672a2513f51b4af19d547fa6a34a747f1f59336
uo288543 <uo288543@uniovi.es>
2024-04-09 19:52:26 +0200

    Fixed app crash

--- android-apk/wiq-infinite-quest/App.js
+++ android-apk/wiq-infinite-quest/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import { WebView } from 'react-native-webview';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  webview: {
+    flex: 1,
+  },
+});
 
 export default function App() {
   return (
-    <WebView
-      source={{ uri: 'http://20.19.89.97:3000/' }}
-      style={{ flex: 1 }}
-    />
+    <View style={styles.container}>
+      <WebView
+        source={{ uri: 'http://20.19.89.97:3000/' }}
+        style={styles.webview}
+      />
+  </View>
   );
 }
