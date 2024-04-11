9cda98ab740a3a05ee6ee6c2bb36bf0273bc2241
uo289689 <uo289689@uniovi.es>
2024-04-11 19:24:37 +0200

    Fixed Statistics Time Registered

--- webapp/src/pages/Statistics.js
+++ webapp/src/pages/Statistics.js
@@ -179,7 +179,7 @@ const Statistics = () => {
 
     const formatCreatedAt = (createdAt) => {
         const date = new Date(createdAt);
-        return date.toLocaleString('en-US', { timeZone: 'UTC', year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', hour12: false });
+        return date.toLocaleString('en-US', { timeZone: 'Europe/Madrid', year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', hour12: false });
     };
 
     const renderQuestions = () => {
