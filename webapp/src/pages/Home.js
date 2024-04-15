5078061cf38d94251168dd7cea2ad48410ec793a
Pablo Barrero Cruz <uo289642@uniovi.es>
2024-04-15 19:37:13 +0200

    New Desing For MainPage

--- webapp/src/pages/Home.js
+++ webapp/src/pages/Home.js
@@ -1,41 +1,81 @@
 import * as React from "react";
-import { Box, Button, Typography, useTheme } from "@mui/material";
+import {Box, Button} from "@mui/material";
 
 const Home = () => {
-    const theme = useTheme();
-
     const styles = {
-        // Constant that stores the styles of the play button
-        playButton : {
-            height: "4rem",
-            width: "13rem",
-            marginTop:'5vh',
-            fontSize:'1.5rem',
-            fontFamily: 'Arial Black, sans-serif',
+        logo:{
+            width: {
+                xs: '25em', // Más pequeño para dispositivos extra pequeños
+                sm: '30em', // Mediano para dispositivos pequeños
+                md: '35em', // Normal para dispositivos medianos
+                lg: '40em', // Grande para dispositivos grandes
+                xl: '45em' // Muy grande para dispositivos extra grandes
+            },
+            marginTop:'3rem',
+            userSelect: 'none',
+            pointerEvents: 'none'
+        },
 
-            color: theme.palette.success.main,
+        playButton: {
+            height: {
+                xs: '3rem', // Tamaño para dispositivos extra pequeños
+                sm: '3.5rem', // Tamaño para dispositivos pequeños
+                md: '4rem', // Tamaño para dispositivos medianos
+                lg: '4.5rem', // Tamaño para dispositivos grandes
+                xl: '5rem' // Tamaño para dispositivos extra grandes
+            },
+            width: {
+                xs: '10rem',
+                sm: '12rem',
+                md: '13.5rem',
+                lg: '14.5rem',
+                xl: '15rem'
+            },
+            fontSize: {
+                xs: '0.8rem',
+                sm: '0.9rem',
+                md: '1rem',
+                lg: '1.1rem',
+                xl: '1.2rem'
+            },
+
+            marginTop: '3rem',
+            marginBottom:'3rem',
+            fontFamily: 'Arial Black, sans-serif',
+            color: 'white',
             backgroundColor: 'transparent',
-            border: `2px solid ${theme.palette.success.main}`,
+            border: `2px solid white`,
             transition: 'background-color 0.3s ease',
 
             '&:hover': {
-            backgroundColor: theme.palette.success.main,
-            color: theme.palette.success.contrastText,
+                backgroundColor: 'white',
+                color: 'black',
             }
         },
+        fullScreen: {
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: 'center',
+            flex: 1,
+            width: '100vw',
+            height: '100vh',
+            margin: 0,
+            padding: 0,
+        }
+    };
 
-    }
     return (
-        <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: 'center', flex:'1', marginX:'10%'}}>
-            <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center",justifyContent: 'center', width:'40%',}}>
-                <img src="./logo_wiq.png" alt="Logo" style={{ maxWidth: "100%"}}/>
-                <Box sx={{ display: "flex", justifyContent: "center", gap: "1em", letterSpacing: "100%" }}>
-                    <Typography align="center" sx={{fontSize: { xs:'1rem', sm: '1.2rem', md: '1.5rem',}, fontWeight: "bold",  color: theme.palette.error.main }}> WIKIDATA </Typography>
-                    <Typography align="center" sx={{fontSize: { xs:'1rem', sm: '1.2rem', md: '1.5rem',}, fontWeight: "bold",  color: theme.palette.success.main }}> INFINITE </Typography>
-                    <Typography align="center" sx={{fontSize: { xs:'1rem', sm: '1.2rem', md: '1.5rem',}, fontWeight: "bold",  color: theme.palette.primary.main }}> QUEST </Typography>
-                </Box>
-                <Button variant='conteined' href={"/login"} sx={styles.playButton}> PLAY </Button>
+        <Box sx={styles.fullScreen}>
+            <video autoPlay muted loop style={{ position: "absolute", width: "100%", height: "100%", objectFit: "cover", zIndex:'-1', userSelect:'none', pointerEvents: 'none'}}>
+                <source src="./home/Background-Blur.mp4" type="video/mp4" />
+            </video>
+
+            <Box sx={{ ...styles.logo, userSelect: 'none', pointerEvents: 'none' }}>
+                <img src="./home/HomeLogo.png" alt="Logo" style={{ width: '100%' }} />
             </Box>
+
+            <Button variant='contained' href={"/login"} sx={styles.playButton}> PLAY </Button>
         </Box>
     );
 };
