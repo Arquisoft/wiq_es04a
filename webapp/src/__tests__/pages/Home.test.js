4f324150995c2471f8dd6280fd2691132a973399
Pablo Barrero Cruz <uo289642@uniovi.es>
2024-04-16 22:33:24 +0200

    HomeTest

--- webapp/src/__tests__/pages/Home.test.js
+++ webapp/src/__tests__/pages/Home.test.js
@@ -1,41 +1,39 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Home from '../../pages/Home';
-import { BrowserRouter  } from 'react-router-dom';
-import { SessionContext } from '../../SessionContext';
-import userEvent from '@testing-library/user-event';
-import '../../localize/i18n';
 
-describe('NavBar component', () => {
+// Hacemos un mock del módulo '@mui/material' y su hook useMediaQuery
+jest.mock('@mui/material/useMediaQuery', () => ({
+    __esModule: true, // Esto es necesario para mocks de módulos ES6
+    default: jest.fn(), // Mock por defecto para todas las llamadas
+  }));
 
-    beforeEach(() => {
-        render(
-            <SessionContext.Provider value={{}}>
-                <BrowserRouter>
-                    <Home/>
-                </BrowserRouter>
-            </SessionContext.Provider>
-        );
+describe('Componente Home', () => {
+    afterEach(() => {
+        jest.restoreAllMocks(); // Limpiar todos los mocks después de cada prueba
+      });
+
+    it('debe aplicar estilos a la foto', () => {
+        render(<Home />);
+        expect(screen.getByAltText('Logo')).toHaveStyle('width: 100%');
     });
 
-    it('should render components', async () => {
-        const logo = screen.getByAltText('Logo');
-        await expect(logo).toBeInTheDocument();
+    it('debe reproducir el video a una velocidad de 0.85', async () => {
+        render(<Home />);
+        const videoElement = screen.getByTestId('video');
+        expect(videoElement).toBeInTheDocument();
+        expect(videoElement.playbackRate).toBe(0.85);
 
-        const name1 = screen.getByText('WIKIDATA');
-        const name2 = screen.getByText('INFINITE');
-        const name3 = screen.getByText('QUEST');
-        await expect(name1).toBeInTheDocument();
-        await expect(name2).toBeInTheDocument();
-        await expect(name3).toBeInTheDocument();
     });
 
-    it('button play functions as expected', async () => {
-        const playButton = screen.getByText('PLAY');
-        await expect(playButton).toBeInTheDocument();
+    it('debe aplicar estilos maxLogo si la media query xxl es verdadera', () => {
+        const useMediaQuery = require('@mui/material/useMediaQuery').default;
+        useMediaQuery.mockImplementation(() => true); // Simula una media query verdadera
 
-        userEvent.click(playButton);
-        expect(window.location.pathname).toBe('/');
-    });
+        render(<Home />);
+        const box = screen.getByTestId('xxl');
+        expect(box).toHaveStyle(`width: 35em`); // Verifica que se aplican los estilos de maxLogo
+      });
+
 });
 
