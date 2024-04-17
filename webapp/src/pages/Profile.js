21be2e2433dd5e6373684b31dee1956e4989acf6
Daniel Andina <uo287791@uniovi.es>
2024-04-17 19:51:11 +0200

    updates in profile

--- webapp/src/pages/Profile.js
+++ webapp/src/pages/Profile.js
@@ -44,46 +44,46 @@ const Profile = () => {
 
     return (
         
-        <Container sx={{ margin: '0 auto auto', display:'flex',flexDirection:'column' }}>
-            <Typography variant="h3" sx={{ textAlign:'center', margin:'1em 0 2em', fontWeight:'bold' }}>{userInfo.username}</Typography>
+        <Container sx={{ margin: '0 auto auto', display:'flex',flexDirection:'column', justifyContent:'space-between', alignContent:'space-between' }}>
+            <Typography variant="h2" sx={{ textAlign:'center', fontWeight:'bold' }}>{userInfo.username}</Typography>
             <Container sx={{ display:'flex' }}>
                 <Container sx={{ display:'flex', flexDirection:'column', justifyContent:'center' }}>
-                    <Typography variant="h4">Nombre: {userInfo.name}</Typography>
-                    <Typography variant="h4">Apellidos: {userInfo.surname}</Typography>
-                    <Typography variant="h4">Created in: {new Date(userInfo.createdAt).toLocaleDateString()}</Typography>
+                    <Typography variant="h4"><b>Name:</b> {userInfo.name}</Typography>
+                    <Typography variant="h4"><b>Surname:</b> {userInfo.surname}</Typography>
+                    <Typography variant="h4"><b>Created in:</b> {new Date(userInfo.createdAt).toLocaleDateString()}</Typography>
                 </Container>    
-                <img src={userInfo.imageUrl} alt="ActualPhoto" sx={{ height:40 }} />
+                <img src={userInfo.imageUrl} alt="ActualPhoto" style={{ flex: 1, maxWidth:'50%', borderRadius:'50%' }} />
             </Container>
 
-            <Typography variant="h3" sx={{ textAlign:'center', margin:'1em 0 2em', fontWeight:'bold'  }}>Choose your avatar</Typography>
+            <Typography variant="h4" sx={{ textAlign:'center', marginTop:'5em', fontWeight:'bold'  }}>Choose your avatar</Typography>
 
             <Container sx={{ display: 'flex', justifyContent: 'space-around' }}>
                 <Button sx={{ borderRadius:'50%', display:'flex', flexDirection:'column' }}>
-                    <img src={'hugoIcon.jpg'} style={{ flex: 1, maxWidth: '100%', borderRadius:'50%' }} alt="Logo" />
+                    <img src={'hugoIcon.jpg'} style={{ flex: 1, maxWidth: '100%', borderRadius:'50%' }} alt="Icon1" />
                     <Typography sx={{color: '#000000', fontWeight:'bold'  }}>HUGH</Typography>
                 </Button>          
                 <Button sx={{ borderRadius:'50%', display:'flex', flexDirection:'column' }}>
-                    <img src={'bertinIcon.jpg'} style={{ flex: 1, maxWidth: '100%', borderRadius:'50%' }} alt="Logo" />
+                    <img src={'bertinIcon.jpg'} style={{ flex: 1, maxWidth: '100%', borderRadius:'50%' }} alt="Icon2" />
                     <Typography sx={{color: '#000000', fontWeight:'bold' }}>ALBERT</Typography>
                 </Button>      
                 <Button sx={{ borderRadius:'50%', display:'flex', flexDirection:'column' }}>
-                    <img src={'wiffoIcon.jpg'} style={{ flex: 1, maxWidth: '100%', borderRadius:'50%' }} alt="Logo" />
+                    <img src={'wiffoIcon.jpg'} style={{ flex: 1, maxWidth: '100%', borderRadius:'50%' }} alt="Icon3" />
                     <Typography sx={{color: '#000000', fontWeight:'bold' }}>PAUL</Typography>
                 </Button>      
                 <Button sx={{ borderRadius:'50%', display:'flex', flexDirection:'column' }}>
-                    <img src={'and1naIcon.jpg'} style={{ flex: 1, maxWidth: '100%', borderRadius:'50%' }} alt="Logo" />
+                    <img src={'and1naIcon.jpg'} style={{ flex: 1, maxWidth: '100%', borderRadius:'50%' }} alt="Icon4" />
                     <Typography sx={{color: '#000000', fontWeight:'bold' }}>DAN</Typography>
                 </Button>
                 <Button sx={{ borderRadius:'50%', display:'flex', flexDirection:'column' }}>
-                    <img src={'samuIcon.jpg'} style={{ flex: 1, maxWidth: '100%', borderRadius:'50%' }} alt="Logo" />
+                    <img src={'samuIcon.jpg'} style={{ flex: 1, maxWidth: '100%', borderRadius:'50%' }} alt="Icon5" />
                     <Typography sx={{color: '#000000', fontWeight:'bold' }}>SAM </Typography>
                 </Button>
                 <Button sx={{ borderRadius:'50%', display:'flex', flexDirection:'column' }}>
-                    <img src={'barreroIcon.jpg'} style={{ flex: 1, maxWidth: '100%', borderRadius:'50%' }} alt="Logo" />
+                    <img src={'barreroIcon.jpg'} style={{ flex: 1, maxWidth: '100%', borderRadius:'50%' }} alt="Icon6" />
                     <Typography sx={{color: '#000000', fontWeight:'bold' }}>PEIBOL</Typography>
                 </Button>
                 <Button sx={{ borderRadius:'50%', display:'flex', flexDirection:'column' }}>
-                    <img src={'teresaIcon.jpg'} style={{ flex: 1, maxWidth: '100%', borderRadius:'50%' }} alt="Logo" />
+                    <img src={'teresaIcon.jpg'} style={{ flex: 1, maxWidth: '100%', borderRadius:'50%' }} alt="Icon7" />
                     <Typography sx={{color: '#000000', fontWeight:'bold' }}>THERESA</Typography>
                 </Button>   
             </Container>
