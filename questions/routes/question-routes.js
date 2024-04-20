c1a721f4bf282a74638c7bffbd25c8e866c1302b
teresagg <uo288347@uniovi.es>
2024-04-20 20:34:52 +0200

    Tested more questions routes

--- questions/routes/question-routes.js
+++ questions/routes/question-routes.js
@@ -68,13 +68,14 @@ router.get('/getQuestionsFromDb/:n/:category/:lang', async(_req, res) => {
     const n = parseInt(_req.params.n, 10);
     const category = _req.params.category;
     const language = _req.params.lang;
+    const questionCount = await dbService.getQuestionCountByCategory(category, language);
 
     //Verify is n is a correct number
     if (isNaN(n) || n <= 0) {
         return res.status(400).json({ error: 'Parameter "n" must be > 0.' });
     }
     
-    if (await dbService.getQuestionCountByCategory(category, language) < n) {
+    /*if (await dbService.getQuestionCountByCategory(category, language) < n) {
         //Must generate n questions (n because some can fail at generation at the moment 18/04)
         await generateQuestionsService.generateQuestions(n + 2, language, category);
         //Do not wait to generate the others
@@ -86,8 +87,44 @@ router.get('/getQuestionsFromDb/:n/:category/:lang', async(_req, res) => {
             dbService.deleteQuestionById(questions[i]._id);
         }
     }
-    res.json(questions);
+    res.json(questions);*/
     
+
+    // 0: Await till it creates 2, creates 50 async and do not delete
+    if (questionCount == 0) {
+        await generateQuestionsService.generateQuestions(2, language, category);
+        generateQuestionsService.generateQuestions(50, language, category);
+        questions = await dbService.getRandomQuestionsByCategory(n, category, language);
+        res.json(questions);
+        
+    // < 50: async creates 10 and do not delete
+    } else if (questionCount < 50) {
+        //Do not wait to generate the others
+        generateQuestionsService.generateQuestions(10, language, category);
+        questions = await dbService.getRandomQuestionsByCategory(n, category, language);
+        res.json(questions);
+
+    // < 100: async creates 5 and delete
+    } else if (questionCount < 100) {
+        generateQuestionsService.generateQuestions(10, language, category);
+        questions = await dbService.getRandomQuestionsByCategory(n, category, language);
+        res.json(questions);
+        if(questions != null) {
+            for(var i = 0; i < questions.length; i++) {
+                dbService.deleteQuestionById(questions[i]._id);
+            }
+        }
+
+    // >= 100: do not create and delete
+    } else {
+        questions = await dbService.getRandomQuestionsByCategory(n, category, language);
+        res.json(questions);
+        if(questions != null) {
+            for(var i = 0; i < questions.length; i++) {
+                dbService.deleteQuestionById(questions[i]._id);
+            }
+        }
+    }
 });
 
 module.exports = router;
