db48c6400baf90e1727554e0f7630c45a900b37f
Pablo <uo265314@uniovi.es>
2024-04-26 22:27:18 +0200

    Refactored pages to clean the directory

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -6,22 +6,22 @@ import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Homepage from './pages/Homepage';
-import Game from './pages/Game';
-import DiscoveringCitiesGame from './pages/DiscoveringCitiesGame';
-import WiseMenStackGame from './pages/WiseMenStackGame';
+import Game from './pages/games/Game';
+import DiscoveringCitiesGame from './pages/games/DiscoveringCitiesGame';
+import WiseMenStackGame from './pages/games/WiseMenStackGame';
 import Groups from './pages/Groups';
 import GroupDetails from './pages/GroupDetails';
 import Statistics from './pages/Statistics';
 import Ranking from './pages/Ranking'
 import Profile from './pages/Profile'
-import MultiplayerRoom from './pages/MultiplayerRoom';
-import MultiplayerGame from './pages/MultiplayerGame';
-import TheChallengeGame from './pages/TheChallengeGame';
-import WarmQuestionGame from './pages/WarmQuestionGame';
+import MultiplayerRoom from './pages/games/MultiplayerRoom';
+import MultiplayerGame from './pages/games/MultiplayerGame';
+import TheChallengeGame from './pages/games/TheChallengeGame';
+import WarmQuestionGame from './pages/games/WarmQuestionGame';
 import {Route, Routes} from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Box } from '@mui/material';
-import PrivateRoute from './PrivateRoute';
+import PrivateRoute from './pages/PrivateRoute';
 import NotFound from './pages/NotFound';
 
 const theme = createTheme({
